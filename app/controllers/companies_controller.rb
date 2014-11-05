48500c0e0983b5fbd8be0d5882402b829d58af1b
Alexander Handler <alexjhandler@gmail.com>
2014-11-05 11:55:06 -0500

    pull dev

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -16,7 +16,9 @@ class CompaniesController < ApplicationController
 	end
 
 	def create
-		@company = Company.create(title: "New Page", user_id: current_user.id)
+		@company = Company.new(company_params) 
+		@company.user_id = User.find(session[:user_id])
+		@company.save
   		redirect_to company_index_path, notice: "New Page Created!"
 	end
 
@@ -38,5 +40,9 @@ class CompaniesController < ApplicationController
 
 	def set_company
 			@company = Company.find(params[:id])
+	end
+
+	def company_params
+		params.require(:company).permit( :company, :tagline, :address1, :address2, :city, :state, :zipcode, :phone, :fax, :email, :about, :hours, :years_establisted, :facebook, :twitter, :tumblr, :linkedin, :package_id, :template_id, :user_id, :subdomain)
 	end
 end
