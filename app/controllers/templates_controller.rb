144d02281d882be7869e6553df18188aba7f78b9
Alexander Handler <alexjhandler@gmail.com>
2014-11-05 14:24:00 -0500

    change

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -3,7 +3,18 @@ class TemplatesController < ApplicationController
 
 	end
 
+	def new
+		@template = Template.new
+	end
+	
 	def create
-		
+		@template = Template.new(template_params)
+		@template.save
+	end
+
+	private
+
+	def template_params 
+		params.require(:template).permit(:template_name)
 	end
 end
