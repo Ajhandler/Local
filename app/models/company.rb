20c3ab96f8be864bcc90eaded43ad63130589316
Alexander Handler <alexjhandler@gmail.com>
2014-11-06 16:39:39 -0500

    fix subdomain bug

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,7 @@ class Company < ActiveRecord::Base
 	
 	belongs_to :user
 	belongs_to :theme
-  is_sluggable :subdomain
+  	# is_sluggable :subdomain
 	has_attached_file :logo, :styles => { :large => "300x300", :medium => "150x150>", :small => "100x100", :thumb => "75x75>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :logo, :content_type => /\Aimage/
 	validates_attachment_file_name :logo, :matches => [/png\Z/, /jpe?g\Z/]
