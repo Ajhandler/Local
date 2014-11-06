189f016d5d3f70334603d0aac928638348fa75c0
Alexander Handler <alexjhandler@gmail.com>
2014-11-06 10:32:37 -0500

    add devise sessions as /login /signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  as :user do
+  get "/login" => "devise/sessions#new"
+  get "/signup" => "devise/sessions#create"
+end
   resources :companies
   resources :themes 
   root 'home#index'
