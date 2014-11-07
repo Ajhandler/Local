c1988533e71c264dee7bd3f9425c6b48c83d18a2
Emerson Scherer <emerson@emersonscherer.com>
2014-11-07 10:22:27 -0500

    Edited Users Index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
 
 	def index
-
+    @users = User.all
 	end
+
+  private
+  def set_company
+    if params[:id].length < 3
+      @company = Company.find(params[:id])
+    else
+      @company = Company.find_by_subdomain(params[:id])
+    end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 end
